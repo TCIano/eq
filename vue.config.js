b8132d69a96c6bceadc6154e38954ecc64bd762a
TCIano <740409310@qq.com>
2025-10-11 15:38:24 +0800

    feat: 更新

--- vue.config.js
+++ vue.config.js
@@ -12,9 +12,7 @@ module.exports = defineConfig({
       '/faultPredict': {
         target: 'http://192.168.2.25:7801/', //请求的服务器地址
         // target: 'http://192.168.0.86:7801/', //请求的服务器地址
-
         // target: 'http://127.0.0.1:4523/m1/6640419-0-default/', //请求的服务器地址
-
         changeOrigin: true,
         // pathRewrite: { '^/faultPredict': '' }, //可以让发过去的请求不带打头
       },
@@ -23,7 +21,6 @@ module.exports = defineConfig({
         changeOrigin: true,
         pathRewrite: { '^/tianwei': '' },
       },
-
     },
   },
 })
